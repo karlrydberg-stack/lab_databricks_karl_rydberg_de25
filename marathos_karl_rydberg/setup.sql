1aadb595ecd2343b589b3b11d1cf64e9a63ec5e5
kalle.rydberg02@gmail.com <kalle.rydberg02@gmail.com>
2026-05-24 12:45:44 +0000

    started eda on marathos, created catalog structure

---
+++ marathos_karl_rydberg/setup.sql
@@ -0,0 +1,9 @@
+CREATE CATALOG IF NOT EXISTS marathos;
+
+CREATE SCHEMA IF NOT EXISTS marathos.default;
+CREATE SCHEMA IF NOT EXISTS marathos.bronze;
+CREATE SCHEMA IF NOT EXISTS marathos.silver;
+CREATE SCHEMA IF NOT EXISTS marathos.gold;
+CREATE SCHEMA IF NOT EXISTS marathos.information_schema;
+
+CREATE VOLUME IF NOT EXISTS marathos.default.raw;
--- marathos_karl_rydberg/setup/create_catalog.py
+++
@@ -1,4 +0,0 @@
-spark.sql("CREATE CATALOG IF NOT EXISTS marathos")
-spark.sql("CREATE SCHEMA IF NOT EXISTS marathos.bronze")
-spark.sql("CREATE SCHEMA IF NOT EXISTS marathos.silver")
-spark.sql("CREATE SCHEMA IF NOT EXISTS marathos.gold")
